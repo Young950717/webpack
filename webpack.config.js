207477bdb60941eaf811ab31260665e3df004be0
Young <634046906@qq.com>
2020-12-17 16:47:02 +0800

    多文件入口配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ const htmlwebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 删除dist的东西
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/index.js',
+    sub: './src/index.js' // 重复打包一次
   },
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].js', // 使用占位符
+    publicPath: 'http://cdn.com/', // 静态资源需要放到cdn上的话 加个公共前缀地址
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'development', // 生产或者开发环境
