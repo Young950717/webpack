52621e99aa68b8b07aaad94bbc2f9cfe542ac079
Young <634046906@qq.com>
2020-12-18 14:48:44 +0800

    feat: webpack打包react.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-// import "@babel/polyfill"
-const arr = [
-  new Promise((reslove, reject) => { }),
-  new Promise((reslove, reject) => { }),
-  new Promise((reslove, reject) => { }),
-  new Promise((reslove, reject) => { })
-]
-
-arr.forEach(i => {
-  console.log(i)
+import "@babel/polyfill"
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+class App extends Component {
+  render () {
+    return <div>hello world</div>
+  }
+}
+ReactDom.render(<App />, document.querySelector('#app'), () => {
+  console.log('123')
 })
+// console.log(document.querySelector('#app'))
